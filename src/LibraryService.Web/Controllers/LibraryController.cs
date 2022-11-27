a99cc63ba3ee2a621a18b4569a838bad758c94dc
Simeon Iksanov <Simeon.Iksanov@gmail.com>
2022-11-27 20:25:43 +0500

    add mvc project

---
+++ src/LibraryService.Web/Controllers/LibraryController.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using LibraryService.Web.Models;
+using LibraryServiceReference;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryService.Web.Controllers;
+public class LibraryController : Controller
+{
+    private readonly ILogger<LibraryController> _logger;
+
+    public LibraryController(ILogger<LibraryController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult IndexAsync(SearchType searchType, string searchString)
+    {
+        LibraryWebServiceSoapClient client =
+            new LibraryWebServiceSoapClient(LibraryWebServiceSoapClient.EndpointConfiguration.LibraryWebServiceSoap12);
+
+        try
+        {
+            if (!string.IsNullOrWhiteSpace(searchString) && searchString.Length >= 3)
+            {
+                switch (searchType)
+                {
+                    case SearchType.Title:
+                        var b = client.GetBooksByTitle(searchString);
+                        return View(new BookCategoryViewModel
+                        {
+                            //Books = await client.GetBooksByTitleAsync(searchString)
+                            Books = b
+                        });
+                    case SearchType.Author:
+                        return View(new BookCategoryViewModel
+                        {
+                            Books = client.GetBooksByAuthor(searchString)
+                        });
+                    case SearchType.Category:
+                        return View(new BookCategoryViewModel
+                        {
+                            Books = client.GetBooksByCategory(searchString)
+                        });
+                }
+
+            }
+        }
+        catch
+        {
+        }
+        return View(new BookCategoryViewModel
+        {
+            Books = Array.Empty<Book>()
+        });
+    }
+
+}
---
+++ src/LibraryService.Web/Models/BookCategoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using LibraryServiceReference;
+
+namespace LibraryService.Web.Models;
+
+public class BookCategoryViewModel
+{
+    public Book[] Books { get; set; }
+    public SearchType SearchType { get; set; }
+    public string SearchString { get; set; }
+}
