e9884ea001c76df7cbd301e3c6df402e7a80abe0
Simeon Iksanov <Simeon.Iksanov@gmail.com>
2022-12-06 22:22:46 +0500

    Lesson03 done

---
+++ Lesson03/ClinicService.Api/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Net;
+using ClinicService.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+namespace ClinicService.Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.WebHost.ConfigureKestrel(options =>
+        {
+            options.Listen(IPAddress.Any, 5001, listenOptions =>
+            {
+                listenOptions.Protocols = HttpProtocols.Http2;
+            });
+        });
+
+        builder.Services.AddGrpc();
+
+        builder.Services.AddDbContext<ClinicServiceDbContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+        }, ServiceLifetime.Scoped);
+
+        #region Automapper
+
+        builder.Services.AddAutoMapper(typeof(Program));
+
+        #endregion
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.UseEndpoints(endpoint =>
+        {
+            endpoint.MapGrpcService<ClinicService.Api.Services.Impl.ClinicService>();
+        });
+
+        app.Run();
+    }
+}
