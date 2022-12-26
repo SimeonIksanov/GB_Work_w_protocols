dc44599a5f8d92ca3963ba8a9414258f9d4b156f
Simeon Iksanov <simeon.iksanov@gmail.com>
2022-12-26 23:59:48 +0500

    hw06 done

---
+++ Lesson06/SampleService/Controllers/WeatherForecastController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RootServiceNamespace;
+using SampleService.Services.Client;
+
+namespace SampleService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private readonly ILogger<WeatherForecastController> _logger;
+    private readonly IRootServiceClient _rootServiceClient;
+
+    public WeatherForecastController(
+        ILogger<WeatherForecastController> logger,
+        IRootServiceClient rootServiceClient)
+    {
+        _logger = logger;
+        _rootServiceClient = rootServiceClient;
+    }
+
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public async Task<IEnumerable<WeatherForecast>> Get()
+    {
+        return await _rootServiceClient.Get();
+    }
+}
+
